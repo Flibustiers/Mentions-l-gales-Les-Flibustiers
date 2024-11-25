0811533d9b643f0d87e0150072eb233e1807d865
Flibustiers <165799310+Flibustiers@users.noreply.github.com>
2024-11-25 15:15:11 +0100

    Update mentions-legales-flibustiers.php

--- mentions-legales-flibustiers.php
+++ mentions-legales-flibustiers.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Mentions Légales
 Description: Plugin pour gérer les mentions légales du site via un shortcode.
-Version: 1.9
+Version: 1.9.1
 Author: Anthony REBOURS - Développeur web chez Les Flibustiers
 */
 
